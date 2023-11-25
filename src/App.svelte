4d8effbaaaca6a044d893e0327c3cb055b5df3a2
onsclom <onsclom@onsclom.net>
2023-11-25 13:21:26 -0800

    Finish minimal landing page

--- src/App.svelte
+++ src/App.svelte
@@ -15,55 +15,99 @@
       .toLocaleLowerCase();
   }
 
-  // get todays date as Mon D, YYYY
   const today = new Date();
   const date = dateToNiceString(today);
-
   const now = new Date();
 
   type Note = { date: string; content: string };
 
-  let notes = [
+  let notes = $state<Note[]>([
     {
-      date: now,
-      content: `take out the trash
-      
-password: hunter2
-
-desk lenght 1.5m`,
+      date: dateToNiceString(now),
+      content: ``.toLocaleLowerCase(),
     },
-  ];
+    {
+      date: dateToNiceString(new Date(now.getTime() - 1000 * 60 * 60 * 24)),
+      content:
+        `Had a productive team meeting this morning, discussing project timelines. Lunch at 'The Green Bowl' was a delight - the salad was fresh and flavorful.
 
-  if (demo) {
-    // lets generate some demo data
+Read more of 'The Alchemist' - it's getting interesting.
+
+Alex's birthday tomorrow, must send a card.
+
+Grateful for: Friends, family, sunny days.`.toLocaleLowerCase(),
+    },
+    {
+      date: dateToNiceString(new Date(now.getTime() - 1000 * 60 * 60 * 24 * 2)),
+      content: `Woke up not feeling great, skipped gym.
+
+Finished marketing proposal draft!
+
+Watched a documentary on minimalism. Very inspiring.
+
+Dinner: Made Spaghetti Aglio e Olio. Yum!
+
+Patience is key in life’s challenges.`.toLocaleLowerCase(),
+    },
+  ]);
+  const todaysNote = $derived(notes[0]!);
+
+  // @ts-ignore
+  function withViewTransition(fn: () => void) {
+    if (!document.startViewTransition) fn();
+    document.startViewTransition(() => {
+      fn();
+    });
   }
 </script>
 
 <main>
-  <div>
-    <span>
-      {date}
-    </span>
+  <div class="top-bar">
+    <i>
+      {mode === "reading" ? `📖 ${notes.length} notes` : date}
+    </i>
     <button
-      onclick={() => {
-        mode = mode === "reading" ? "writing" : "reading";
-      }}
+      onclick={() =>
+        withViewTransition(() => {
+          mode = mode === "reading" ? "writing" : "reading";
+        })}
     >
-      {mode === "reading" ? "write" : "read"}
+      {mode === "reading" ? "✏️ write" : "📖 read"}
     </button>
   </div>
 
   {#if mode === "reading"}
-    {#each notes as note (note.date)}
-      <h2>{date}</h2>
-      <pre>{note.content}</pre>
-    {/each}
+    <div class="note-container">
+      {#each notes as note (note.date)}
+        <div>
+          <h2>{note.date}</h2>
+          <pre>{note.content}</pre>
+        </div>
+      {/each}
+      <i style="text-align: center"
+        >you have reached the end of your notes! 🏖️</i
+      >
+    </div>
   {:else}
-    <textarea placeholder="write here..."></textarea>
+    <textarea placeholder="write here..." bind:value={todaysNote.content}
+    ></textarea>
   {/if}
 </main>
 
 <style>
+  pre {
+    font-size: medium;
+    white-space: pre-wrap;
+  }
+
+  .note-container {
+    overflow: auto;
+    padding: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
   main {
     height: 100%;
     display: flex;
@@ -71,18 +115,20 @@ desk lenght 1.5m`,
   }
 
   main > textarea {
+    display: block;
     flex: 1;
-    height: 100%;
     border: 0;
     box-sizing: border-box;
     padding: 0.5rem;
     resize: none;
+    font-size: medium;
   }
 
-  main > div {
+  .top-bar {
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 0.5rem;
+    border-bottom: 1px solid #ccc;
   }
 </style>
